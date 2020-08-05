8aed8d1a8ef710f7c8f1ffb25d77f9cafe9f217d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-08-06 11:51:01 +1200

    pass homeBudget array as props to BudgetOverview component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import BudgetOverview from "./components/BudgetOverview";
 import "./App.css";
 
+const homeBudget = [
+  {
+    budgeted: 500,
+    spent: 200,
+    category: "Food",
+  },
+  {
+    budgeted: 1000,
+    spent: 1500,
+    category: "Utilities",
+  },
+  {
+    budgeted: 500,
+    spent: 300,
+    category: "Shopping",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <BudgetOverview />
+      <BudgetOverview budgets={homeBudget} />
     </div>
   );
 }
